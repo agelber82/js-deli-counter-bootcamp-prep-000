4a9af88c2ada099fd732a979b74b431630deebec
alandgelber@gmail.com <alandgelber@gmail.com>
2018-02-16 23:55:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,9 @@ function currentLine(katzDeli) {
    for (let i = 0; i < katzDeli.length; i++) {
      line.push(` `+[i+1]+`. `  + katzDeli[i]);
    }
+   if (katzDeli.length === 0) {
+     return "The line is currently empty.";
+   }
+   else
    return(`The line is currently:` + line);
    }
